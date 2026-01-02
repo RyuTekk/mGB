7e245396ccca0d3a853fe69a01437207e322fc9b
Ced <Ced2911@users.noreply.github.com>
2026-01-02 10:02:01 +0000

    hack to add pokcart support

--- Source/io/midi.c
+++ Source/io/midi.c
@@ -8,7 +8,12 @@ uint8_t addressByte;
 uint8_t valueByte;
 uint8_t capturedAddress;
 
-void updateMidiBuffer(void) {
+static volatile uint8_t *MIDI_IN = (volatile uint8_t *)0xB000;
+static volatile uint8_t *MIDI_PACKET = (volatile uint8_t *)0xB001;
+
+void updateMidiBuffer(void)
+{
+#if 0
   if (serialBufferReadPosition == serialBufferPosition) {
     return;
   }
@@ -58,4 +63,53 @@ void updateMidiBuffer(void) {
     asmEventMidiPC();
     break;
   }
+#else
+
+  ENABLE_RAM_MBC1;
+  SWITCH_RAM_MBC1(0);
+  SWITCH_16_8_MODE_MBC1;
+
+  // packets are sent in usb midi packet fmt
+  // 0: packet len
+  // 1: CIN/CABLE
+  // 2: status
+  // 3: d0
+  // 4: d1
+
+  if (MIDI_IN[0] != 0)
+  {
+    // data received, block ui
+    systemIdle = false;
+
+    statusByte = MIDI_IN[2];
+    addressByte = MIDI_IN[3];
+    valueByte = MIDI_IN[4];
+    
+    // stop recv
+    MIDI_IN[0] = 0;
+
+    switch ((statusByte >> 4) & 0x0F)
+    {
+    case MIDI_STATUS_PB:
+      asmEventMidiPB();
+      break;
+    case MIDI_STATUS_CC:
+      asmEventMidiCC();
+      break;
+    case MIDI_STATUS_NOTE_ON:
+      asmEventMidiNote();
+      break;
+    case MIDI_STATUS_NOTE_OFF:
+      asmEventMidiNoteOff();
+      break;
+    case MIDI_STATUS_PC:
+      asmEventMidiPC();
+      break;
+    }
+    serialBufferPosition++;
+  }
+
+  DISABLE_RAM_MBC1;
+
+#endif
 }
